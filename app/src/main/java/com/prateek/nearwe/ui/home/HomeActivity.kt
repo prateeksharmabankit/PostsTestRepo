bceb833db7cf2609c0fdf03d7c2f823d549d10f1
Prateek Sharma <prateek.sharma@bankit.in>
2022-06-12 11:54:50 +0530

    Commited With Final Code

--- app/src/main/java/com/prateek/nearwe/ui/home/HomeActivity.kt
+++ app/src/main/java/com/prateek/nearwe/ui/home/HomeActivity.kt
@@ -1,4 +1,10 @@
 /*************************************************
+ * Created by Efendi Hariyadi on 12/06/22, 11:14 AM
+ * Copyright (c) 2022 . All rights reserved.
+ * Last modified 12/06/22, 11:05 AM
+ ************************************************/
+
+/*************************************************
  * Created by Efendi Hariyadi on 11/06/22, 1:47 PM
  * Copyright (c) 2022 . All rights reserved.
  * Last modified 11/06/22, 12:08 AM
@@ -10,35 +16,47 @@
  * Last modified 02/06/22, 10:02 PM
  ************************************************/
 
-package com.prateek.nearwe
+package com.prateek.nearwe.ui.home
 
 import android.os.Bundle
 import com.google.android.material.tabs.TabLayout
 import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupWithNavController
 import androidx.work.*
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.prateek.nearwe.databinding.ActivityHomeBinding
 import com.prateek.nearwe.ui.adapters.PostsViewPagerAdapter
 import com.prateek.nearwe.workmanager.FavouritesScheduler
 import java.util.concurrent.TimeUnit
+import com.prateek.nearwe.R;
 
 class HomeActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityHomeBinding
-
+    //private lateinit var binding: ActivityHomeBinding
+    private lateinit var navController: NavController
+    private lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityHomeBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        // binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_home)
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+        navController = Navigation.findNavController(this, R.id.activity_main_nav_host_fragment)
 
-        val sectionsPagerAdapter = PostsViewPagerAdapter(this, supportFragmentManager)
-        val viewPager: ViewPager = binding.viewPager
-        viewPager.adapter = sectionsPagerAdapter
-        val tabs: TabLayout = binding.tabs
-        tabs.setupWithViewPager(viewPager)
-        //TODO Scheduled Work manager For Pushing Favourites on Ssrver
-        scheduleFavouriteSyncToServer()
+
+        setupWithNavController(bottomNavigationView, navController)
+
+        /* val sectionsPagerAdapter = PostsViewPagerAdapter(this, supportFragmentManager)
+         val viewPager: ViewPager = binding.viewPager
+         viewPager.adapter = sectionsPagerAdapter
+         val tabs: TabLayout = binding.tabs
+         tabs.setupWithViewPager(viewPager)
+         //TODO Scheduled Work manager For Pushing Favourites on Ssrver
+         scheduleFavouriteSyncToServer()*/
 
 
     }
--- app/src/main/java/com/prateek/nearwe/ui/login/LoginActivity.kt
+++ app/src/main/java/com/prateek/nearwe/ui/login/LoginActivity.kt
@@ -18,12 +18,11 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.tasks.Task
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
-import com.prateek.nearwe.HomeActivity
+import com.prateek.nearwe.ui.home.HomeActivity
 import com.prateek.nearwe.R
 import com.prateek.nearwe.api.models.User.UserModel
 
